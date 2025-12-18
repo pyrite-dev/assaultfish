58de3bbbebcd534c5c61eb28b373a36ed8f85334
NishiOwO <nishi@nishi.boats>
2025-12-19 03:08:40 +0900

    wow

--- client/main.c
+++ client/main.c
@@ -3,6 +3,25 @@
 MwWidget root, window, opengl;
 
 static void resize(MwWidget handle, void* user, void* client){
+	int ww = MwGetInteger(window, MwNwidth);
+	int wh = MwGetInteger(window, MwNheight);
+
+	MwVaApply(opengl,
+		MwNwidth, ww,
+		MwNheight, wh,
+	NULL);
+
+	glViewport(0, 0, ww, wh);
+}
+
+static void tick(MwWidget handle, void* user, void* client){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	MwOpenGLSwapBuffer(opengl);
+}
+
+static void* gl_load(const char* name){
+	return MwOpenGLGetProcAddress(opengl, name);
 }
 
 int main(){
@@ -20,7 +39,12 @@ int main(){
 
 	MwOpenGLMakeCurrent(opengl);
 
+	gladLoadGLLoader(gl_load);
+
+	resize(window, NULL, NULL);
+
 	MwAddUserHandler(window, MwNresizeHandler, resize, NULL);
+	MwAddUserHandler(root, MwNtickHandler, tick, NULL);
 
 	MwLoop(root);
 }
