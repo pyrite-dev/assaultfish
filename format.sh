81570ca99e38d1c70fd0e8fc6759b4d32765ec15
NishiOwO <nishi@nishi.boats>
2026-01-12 06:37:30 +0900

    fix format.sh

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clang-format --verbose -i `find engine/src engine/include engine/net engine/tools '(' -name "*.c" -or -name "*.h" ')' -and -not -name "stb_*.h" | sed 's%engine/include/GearSrc/GL/GL.h%%g'`
+clang-format --verbose -i `find engine/src engine/include engine/tools '(' -name "*.c" -or -name "*.h" ')' -and -not -name "stb_*.h" | sed 's%engine/include/GearSrc/GL/GL.h%%g'`
